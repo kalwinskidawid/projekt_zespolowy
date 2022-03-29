eda99bf930c63d854b6e9d90dbdd4a42c2831727
wojciechkonieczny <wojciech9929@gmail.com>
2022-03-29 19:56:39 +0200

    Dodanie paczki językowej 'pl'

---
+++ resources/lang/pl/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'auth' => [
+        'reset_send' => 'Link do zresetowania hasła został wysłany',
+        'invalid_token' => 'Niepoprawny token',
+        'password_changed' => 'Twoje hasło zostało zmienione.'
+    ]
+
+];
