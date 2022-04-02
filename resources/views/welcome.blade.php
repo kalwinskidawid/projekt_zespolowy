8f76cd0b0762a0868b6034aa4d334b7bbc7ed799
Pasozyt <57331667+Pasozyt@users.noreply.github.com>
2022-04-02 17:18:21 +0200

    Rejestracja użytkownika z wyborem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,11 +32,9 @@
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-<!-- od komentować gdy bedziemy robic rejestracje xDDDD
-                        if (Route::has('register'))
+                        @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        endif
--->
+                        @endif
                     @endauth
                 </div>
             @endif
